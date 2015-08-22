cb386d59d21aec579f096d4baaf3c45a6ecc4f3f
Ilya Siganov <ilya.blan4@gmail.com>
2015-08-22 16:56:59 +0600

    add units seq

--- src/main/scala/com/catinthedark/sszb/DayState.scala
+++ src/main/scala/com/catinthedark/sszb/DayState.scala
@@ -2,15 +2,14 @@ package com.catinthedark.sszb
 
 import com.badlogic.gdx.{Input, Gdx}
 import com.catinthedark.sszb.common.Const.Distance
-import com.catinthedark.sszb.lib.{Interval, LocalDeferred, YieldUnit}
-import com.catinthedark.sszb.units.Control
-import com.catinthedark.sszb.units.HudView
+import com.catinthedark.sszb.lib.{SimpleUnit, Interval, LocalDeferred, YieldUnit}
+import com.catinthedark.sszb.units.{AI, AIControl, Control, HudView}
 
 /**
  * Created by over on 18.04.15.
  */
 class DayState(shared: Shared) extends YieldUnit[Boolean] {
-  val hud = new HudView(shared)
+  var units: Seq[SimpleUnit] = Seq()
   
   override def toString = "Day"
   val control = new Control(shared) with LocalDeferred with Interval {
@@ -19,12 +18,17 @@ class DayState(shared: Shared) extends YieldUnit[Boolean] {
   override def onActivate(): Unit = {
     shared.prepareGame()
     control.onActivate()
+    val ai = new AI(shared) with LocalDeferred
+    val aiControl = new AIControl(shared)
+    val hud = new HudView(shared)
+    
+    units = Seq(control, ai, aiControl, hud)
   }
   override def onExit(): Unit = {
-    hud.onExit()
+    units.foreach(_.onExit())
   }
   override def run(delta: Float): Option[Boolean] = {
-    hud.run(delta)
+    units.foreach(_.run(delta))
     shared.lvlTime += delta
     
     if (shared.lvlDistance >= Distance.levelDistance) {
